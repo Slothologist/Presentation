2cb1218ae8beff043a4a33da1c3174101e79aeba
Robert Feldhans <rfeldhans@techfak.uni-bielefeld.de>
2016-12-15 00:24:23 +0100

    python script zum schnellen Darstellen der Versuchsergebnisse

---
+++ Beispiel/Livetest/diag.py
@@ -0,0 +1,57 @@
+import matplotlib.pyplot as pp
+import sys
+
+if len(sys.argv) < 2:
+	achsenwechsel = 1
+	name = 'data.txt'
+elif len(sys.argv) == 2:
+	achsenwechsel = int(sys.argv[1])
+	name = 'data.txt'
+else:
+	achsenwechsel = int(sys.argv[1])
+	name = sys.argv[2]
+
+zu_plotten1 = []
+zu_plotten2 = []
+
+data = [[],[],[],[],[],[]]
+
+with open(name, 'r') as file:
+	for line in file:
+		for (index,wert) in enumerate(line.split(',')):
+			if index == 0:
+				continue
+			data[index-1].append(float(wert))
+
+if achsenwechsel:
+	for each in data[0:3]:
+		zu_plotten1.append(sum(each)/len(each))
+
+	for each in data[3:]:
+		zu_plotten2.append(sum(each)/len(each))
+
+	pp.plot(zu_plotten1, 'r')
+	pp.plot(zu_plotten1, 'r-')
+	pp.plot(zu_plotten2, 'b')
+	pp.plot(zu_plotten2, 'bo')
+	pp.xlabel('Sustantiv     Verb     Adjektiv')
+
+else:
+	zu_plotten3 = []
+	zu_plotten1.append(sum(data[0])/len(data[0]))
+	zu_plotten1.append(sum(data[3])/len(data[3]))
+	zu_plotten2.append(sum(data[1])/len(data[1]))
+	zu_plotten2.append(sum(data[4])/len(data[4]))
+	zu_plotten3.append(sum(data[2])/len(data[2]))
+	zu_plotten3.append(sum(data[5])/len(data[5]))
+	pp.plot(zu_plotten1, 'r')
+	pp.plot(zu_plotten1, 'r-')
+	pp.plot(zu_plotten2, 'b')
+	pp.plot(zu_plotten2, 'bo')
+	pp.plot(zu_plotten3, 'g')
+	pp.plot(zu_plotten3, 'g^')
+	pp.xlabel('Durchgang 1     Durchgang 2')
+
+pp.ylabel('# richtige Worte')
+pp.grid(True)
+pp.show()
